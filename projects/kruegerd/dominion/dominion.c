bab98c31417bef6efffaea72f123cba82d8d292c
dwkrueger2 <dwkrueger2@users.noreply.github.com>
2018-10-12 17:13:56 -0400

    Removed some unused variables

--- projects/kruegerd/dominion/dominion.c
+++ projects/kruegerd/dominion/dominion.c
@@ -652,7 +652,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 {
 	int i;
 	int j;
-	int k;
+	//int k;
 	int x;
 	int index;
 	int currentPlayer = whoseTurn(state);
@@ -660,8 +660,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
 	int tributeRevealedCards[2] = { -1, -1 };
 	int temphand[MAX_HAND];// moved above the if statement
-	int drawntreasure = 0;
-	int cardDrawn;
+	//int drawntreasure = 0;
+	//int cardDrawn;
 	//int z = 0;// this is the counter for the temp hand
 	if (nextPlayer > (state->numPlayers - 1)) {
 		nextPlayer = 0;
